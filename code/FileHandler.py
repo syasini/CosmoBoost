f0c6018a3e111cd48f42e90f695c09eebaf6a1a1
Siavash Yasini <yasini@usc.edu>
2017-12-29 16:36:12 -0800

    storage scheme of the matrices has been changed. Now the kernels are saved on a file separate from the other matrices (Blm,Mmat,Lmat,etc.)

--- code/FileHandler.py
+++ code/FileHandler.py
@@ -19,71 +19,133 @@ def dirname(beta,lmax):
     return  COSMOBOOST_DIR+"/data/beta_"+str(beta)[2:]+"/lmax_"+str(lmax)
 
 
-def filename(s,lmax,delta_ell,beta):
-    '''returns the name of the fits file based on params'''
-    return dirname(beta,lmax)+"/d=1_s="+str(s)+"_delta"+str(delta_ell)+"_lmax"+str(lmax)+"_beta"+str(beta)[2:]+".fits"
+def kernel_filename(s,delta_ell,beta,lmax):
+    '''returns the name and address of the fits file based on params'''
+    return dirname(beta,lmax)+"/K_d=1_s="+str(s)+"_delta"+str(delta_ell)+"_lmax"+str(lmax)+"_beta"+str(beta)[2:]+".fits"
+
+def matrices_filename(s,delta_ell,beta,lmax):
+    '''returns the name and address of the fits file based on params'''
+    return dirname(beta,lmax)+"/M_d=1_s="+str(s)+"_delta"+str(delta_ell)+"_lmax"+str(lmax)+"_beta"+str(beta)[2:]+".fits"
 
 
-def init_fits(file_name):
-    '''initialize a fits file with 6 HDUs in the following order:
-    PRIMARY (K): the aberration kernel matrix
-    Blm : the Blm matrix, used in the ODE 
-    M : Mmatrix used in doppler weight lifting of the kernel matrix
-    L : Lmatrix used in doppler weight lifting of the kernel matrix
-    Lp: Lpmatrix used in doppler weight lifting of the kernel matrix
-    S: Smatrix used in doppler weight lifting of the kernel matrix with s!=0
+def init_kernel_fits(kernel_file_name):
+    '''initialize a fits file with 5 HDUs in the following order:
+    PRIMARY (T): the aberration kernel matrix for T
+    E : the aberration kernel matrix for polarization E mode
+    B : the aberration kernel matrix for polarization B mode
+    SP2 : the aberration kernel matrix for polarization s = +2
+    SM2 : the aberration kernel matrix for polarization s = -2
     '''
     
     #setup all 6 HDUs with their respective keywords
     #*note that the keyword for the aberration kernel matrix is "primary"
-    k_hdu = fits.PrimaryHDU()
-    b_hdu = fits.ImageHDU(name="B")
-    m_hdu = fits.ImageHDU(name="M")
-    l_hdu = fits.ImageHDU(name="L")
-    lp_hdu = fits.ImageHDU(name="Lp")
-    s_hdu = fits.ImageHDU(name="S")
+    T_hdu = fits.PrimaryHDU()
+    T_hdu.name = "T"
+    E_hdu = fits.ImageHDU(name="E")
+    B_hdu = fits.ImageHDU(name="B")
+    SP2_hdu = fits.ImageHDU(name="SP2")
+    SM2_hdu = fits.ImageHDU(name="SM2")
     
-    hdus = [k_hdu,b_hdu,m_hdu,l_hdu,lp_hdu,s_hdu]
+    hdus = [T_hdu,E_hdu,B_hdu,SP2_hdu,SM2_hdu]
     
     #concatenate the HDUs into an HDUList and write to fits file
     hdulist = fits.HDUList(hdus)
-    hdulist.writeto(str(file_name),overwrite=True)
+    hdulist.writeto(str(kernel_file_name),overwrite=True)
 
+def init_matrices_fits(matrices_file_name):
+    '''initialize a fits file with 6 HDUs in the following order:
+        PRIMARY (M): Mmatrix used in doppler weight lifting of the kernel matrix
+        Lp: Lpmatrix used in doppler weight lifting of the kernel matrix
+        L : Lmatrix used in doppler weight lifting of the kernel matrix
+        CS0 : Clm matrix for s=0.
+        CS2: Clm matrix for s=2.
+        '''
+    
+    #setup all 6 HDUs with their respective keywords
+    #*note that the keyword for the aberration kernel matrix is "primary"
+    M_hdu = fits.PrimaryHDU()
+    M_hdu.name = "M"
+    LP_hdu = fits.ImageHDU(name="LP")
+    L_hdu = fits.ImageHDU(name="L")
+    CS0_hdu = fits.ImageHDU(name="CS0")
+    CS2_hdu = fits.ImageHDU(name="CS2")
+    S_hdu = fits.ImageHDU(name="S")
+    
+    hdus = [M_hdu,LP_hdu,L_hdu,CS0_hdu,CS2_hdu,S_hdu]
+    
+    #concatenate the HDUs into an HDUList and write to fits file
+    hdulist = fits.HDUList(hdus)
+    hdulist.writeto(str(matrices_file_name),overwrite=True)
 
-def save_matrix(file_name, matrix, key='k',overwrite=False):
+def file_exists(file_name):
+    '''check to see if the fits file exists in the given address'''
+    
+    return os.path.isfile(str(file_name))
+
+def save_kernel(kernel_file_name, kernel, key='T',overwrite=False):
+    '''saves the kernel chosen by 'key' to the fits file
+        initializes the fits file if it doesn't already exist'''
+    
+    
+    #check to see if the file exists
+    file_exists = os.path.isfile(str(kernel_file_name))
+    if (not file_exists or overwrite==True):
+        #initialize the fits file if it doesn't already exist
+        warnings.warn("initializing fits file for the kernel...")
+        init_kernel_fits(kernel_file_name)
+    
+    #open the file in update mode and write the kernel in the appropriate HDU, then close it
+    kernel_hdul = fits.open(str(kernel_file_name),mode='update')
+    kernel_hdul[key].data = kernel
+    kernel_hdul.close()
+
+def save_matrices(matrices_file_name, matrix, key='M',overwrite=False):
     '''saves the matrix chosen by 'key' to the fits file
     initializes the fits file if it doesn't already exist'''
     
-    #change the key from 'k' to 'primary for the aberration kernel matrix
-    if (key=='k' or key=='K'): key='primary'
     
     #check to see if the file exists
-    file_exists = os.path.isfile(str(file_name))
+    file_exists = os.path.isfile(str(matrices_file_name))
     if (not file_exists or overwrite==True):
         #initialize the fits file if it doesn't already exist
         warnings.warn("initializing fits file...")
-        init_fits(file_name)
+        init_matrices_fits(matrices_file_name)
     
     #open the file in update mode and write the matrix in the appropriate HDU, then close it
-    kernel_hdul = fits.open(str(file_name),mode='update')
-    kernel_hdul[key].data = matrix 
+    kernel_hdul = fits.open(str(matrices_file_name),mode='update')
+    kernel_hdul[key].data = matrix
     kernel_hdul.close()
 
 
-def load_matrix(file_name, key='k'):
+def load_kernel(kernel_file_name, key='T'):
     '''loads the matrix chosen by 'key' from fits file'''
     
-    #for the aberration kernel matrix change the key 'k' to primary
-    if (key=='k' or key=='K'): key='primary'
     
     # if the file exists, open it and read the HDU chosen by 'key'
     try:
-        kernel_hdul = fits.open(str(file_name),mode='readonly')
+        kernel_hdul = fits.open(str(kernel_file_name),mode='readonly')
         matrix = kernel_hdul[key].data
         kernel_hdul.close()
 
         return matrix
 
+    #raise error if the file does not exist
+    except IOError:
+        print str(file_name)+" does not exist."
+
+def load_matrix(matrices_file_name, key='M'):
+    '''loads the matrix chosen by 'key' from fits file'''
+    
+    
+    
+    # if the file exists, open it and read the HDU chosen by 'key'
+    try:
+        kernel_hdul = fits.open(str(matrices_file_name),mode='readonly')
+        matrix = kernel_hdul[key].data
+        kernel_hdul.close()
+        
+        return matrix
+    
     #raise error if the file does not exist
     except IOError:
         print str(file_name)+" does not exist."
