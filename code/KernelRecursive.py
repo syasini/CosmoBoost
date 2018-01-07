b75c083a2b1aa29d320a5965c36d7771d5497198
Siavash Yasini <yasini@usc.edu>
2018-01-07 13:59:25 -0800

    K_d_lift function now saves and loads the mid range d-weight kernel matrices to avoid recalculation.

--- code/KernelRecursive.py
+++ code/KernelRecursive.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul  2 17:03:33 2017
-
-@author: siavashyasini
-"""
+    Created on Sun Jul  2 17:03:33 2017
+    
+    @author: siavashyasini
+    """
 
 
 import numpy as np
@@ -13,52 +13,60 @@ import FrequencyFunctions as ff
 import MatrixHandler as mh
 from scipy.misc import derivative, factorial, comb
 from scipy.special import sph_harm as Ylm
-from scipy.integrate import quad 
+from scipy.integrate import quad
 
+sign_pref={0:"0",1:"+",-1:"-"}
 
-
-def _K_d_lift(K,d,s_sign=+1):
+def _K_d_lift(K,d,s):
     
     if d == 1:
         return K._mlpl_d1
-    
-    elif (d>1): 
-        K_d_minusone = _K_d_lift(K,d-1)
-        
-        K_l_plusone_d_minusone = mh.shift_left(K_d_minusone)
-        C_l_plusone = mh.shift_left(K.Cmatrix)
-        
-        K_l_minusone_d_minusone = mh.shift_right(K_d_minusone)
-        
-        
-        return K.gamma*K_d_minusone + K.gamma*K.beta*(  C_l_plusone* K_l_plusone_d_minusone\
-                    + s_sign*K.Smatrix*K_d_minusone + K.Cmatrix*K_l_minusone_d_minusone  )
-    
-    
-    
-def K_d(K,d):
+
+    elif (d>1):
+        key = sign_pref[np.sign(s)]+"d"+str(d)
+        print key
+        try:
+            K_d = fh.load_matrix(K.kernel_filename,key=key)
+        except:
+            K_d_minusone = _K_d_lift(K,d-1,s)
+            
+            K_l_plusone_d_minusone = mh.shift_left(K_d_minusone)
+            C_l_plusone = mh.shift_left(K.Cmatrix)
+            
+            K_l_minusone_d_minusone = mh.shift_right(K_d_minusone)
+            
+            
+            K_d= K.gamma*K_d_minusone + K.gamma*K.beta*(  C_l_plusone* K_l_plusone_d_minusone\
+                                                      + np.sign(s)*K.Smatrix*K_d_minusone + K.Cmatrix*K_l_minusone_d_minusone)
+                                                        
+            fh.append_kernel(K.kernel_filename,K_d,key=key)
+
+        return K_d
+
+
+def K_d(K,d,s):
     '''calculate the Kernel of doppler weight d, from the doppler weight 1, using the recursive expressions '''
     if d>=1:
-        K_d_mlpl = _K_d_lift(K,d)
+        K_d_mlpl = _K_d_lift(K,d,s)
         return K_d_mlpl
-        
+    
     elif d<1:
-        K_d_mlpl = _K_d_lift(K,2-d,s_sign=-1)
+        K_d_mlpl = _K_d_lift(K,2-d,-s)
         print d
         K_d_mllp = mh.transpose(K_d_mlpl,K.delta_ell)
-            
+    
     return mh.minus_one_LplusLp(K.delta_ell,K.lmax)*K_d_mlpl
-        
+
 
 def d2indx(d,i):
     return d-i
 
 
-def get_K_d_arr(K,d):
+def get_K_d_arr(K,d,s):
     height, width = ((K.lmax+1)*(K.lmax+2)/2,2*K.delta_ell+1)
     K_d_arr = np.zeros((K.beta_exp_order+1,height,width))
     for i in range(d,d-K.beta_exp_order-1,-1):
-        K_d_arr[d2indx(d,i)] = K_d(K,i)
+        K_d_arr[d2indx(d,i)] = K_d(K,i,s)
     
     return K_d_arr
 
@@ -79,11 +87,11 @@ def K_nu_d(K_d_arr,nu,pars,freq_func=ff.F_nu,dmod="sp"):
         for k in xrange(beta_order+1):
             Klplm=K_d_arr[k]
             kfactor +=  Klplm *(-1.0)**(beta_order+k) * comb(beta_order,k)
-            
-        Kernel += kfactor/factorial(beta_order)*nu**beta_order * derivative(freq_func,nu,dx =dx,n=beta_order,args=(T,),order=21) # calculate the derivative with scipy
         
-            
-    #Kernel[abs(Kernel)<1.0E-12]=0             
+        Kernel += kfactor/factorial(beta_order)*nu**beta_order * derivative(freq_func,nu,dx =dx,n=beta_order,args=(T,),order=21) # calculate the derivative with scipy
+    
+    
+    #Kernel[abs(Kernel)<1.0E-12]=0
     return Kernel
 
 
@@ -103,11 +111,11 @@ def K_nu_d_norm(K_d_arr,nu,pars,freq_func=ff.F_nu,dmod="sp"):
         for k in xrange(beta_order+1):
             Klplm=K_d_arr[k]
             kfactor +=  Klplm *(-1.0)**(beta_order+k) * comb(beta_order,k)
-            
-        Kernel += kfactor/factorial(beta_order)*nu**beta_order * derivative(freq_func,nu,dx =dx,n=beta_order,args=(T,),order=21) # calculate the derivative with scipy
         
-            
-    #Kernel[abs(Kernel)<1.0E-12]=0             
+        Kernel += kfactor/factorial(beta_order)*nu**beta_order * derivative(freq_func,nu,dx =dx,n=beta_order,args=(T,),order=21) # calculate the derivative with scipy
+    
+    
+    #Kernel[abs(Kernel)<1.0E-12]=0
     return np.true_divide(Kernel,ff.F_nu(nu,pars['T_0']))
 
 
@@ -122,9 +130,10 @@ def Kerd_intgrl_method(lp,l,m,d,beta,gamma):
 
 
 
-def Ker_nu_d_intgrl_method(nup,lp,l,m,d): 
+def Ker_nu_d_intgrl_method(nup,lp,l,m,d):
     beta = 0.001
     gamma = 1.0/np.sqrt(1-beta**2)
     func = lambda mup: np.conj(Ylm(m,lp,0,np.arccos(mup)))*\
         Ylm(m,l,0,np.arccos((mup-beta)/(1-beta*mup)))/(gamma*(1-beta*mup))**d * F_nu(gamma*(1-beta*mup)*nup,T_0)
     return 2*np.pi*quad(func,-1,1)[0]
+
