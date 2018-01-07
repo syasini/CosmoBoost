8ddfeb4a49b88bb5b1ab2e6459e2d4165bb3a145
Siavash Yasini <yasini@usc.edu>
2018-01-07 13:45:58 -0800

    FileHandler.py now only saves the KT (D1) kernel in the kernel_fits.

--- code/FileHandler.py
+++ code/FileHandler.py
@@ -1,9 +1,9 @@
 '''
-*********************FileHandler.py***********************
-includes functions for handling (saving/loading) fits files
-
-@author: siavashyasini
-'''
+    *********************FileHandler.py***********************
+    includes functions for handling (saving/loading) fits files
+    
+    @author: siavashyasini
+    '''
 
 import numpy as np
 from astropy.io import fits
@@ -15,7 +15,7 @@ from cosmoboost import COSMOBOOST_DIR
 
 
 #######################################################
-#             file and directory names 
+#             file and directory names
 #######################################################
 
 
@@ -28,14 +28,14 @@ def kernel_filename(pars):
     '''returns the name and address of the fits file based on params'''
     
     return dirname(pars['beta'],pars['lmax'])+ \
-            "/K_d=1_s="+str(pars['s']) +"_delta"+str(pars['delta_ell'])+\
-            "_lmax"+str(pars['lmax'])+"_beta"+str(pars['beta'])[2:]+".fits"
+        "/K_T_s="+str(np.abs(pars['s'])) +"_delta"+str(pars['delta_ell'])+\
+        "_lmax"+str(pars['lmax'])+"_beta"+str(pars['beta'])[2:]+".fits"
 
 def matrices_filename(pars):
     '''returns the name and address of the fits file based on params'''
     return dirname(pars['beta'],pars['lmax'])+ \
-            "/M_d=1_s="+str(pars['s']) +"_delta"+str(pars['delta_ell'])+\
-            "_lmax"+str(pars['lmax'])+"_beta"+str(pars['beta'])[2:]+".fits"
+        "/M_s="+str(np.abs(pars['s'])) +"_delta"+str(pars['delta_ell'])+\
+        "_lmax"+str(pars['lmax'])+"_beta"+str(pars['beta'])[2:]+".fits"
 
 def file_exists(file_name):
     '''check to see if the fits file exists in the given address'''
@@ -49,14 +49,14 @@ def file_exists(file_name):
 #######################################################
 
 
-def init_kernel_fits(kernel_file_name):
+def init_kernel_fits_temp(kernel_file_name):
     '''initialize a fits file with 5 HDUs in the following order:
-    PRIMARY (T): the aberration kernel matrix for T
-    E : the aberration kernel matrix for polarization E mode
-    B : the aberration kernel matrix for polarization B mode
-    SP2 : the aberration kernel matrix for polarization s = +2
-    SM2 : the aberration kernel matrix for polarization s = -2
-    '''
+        PRIMARY (T): the aberration kernel matrix for T
+        E : the aberration kernel matrix for polarization E mode
+        B : the aberration kernel matrix for polarization B mode
+        SP2 : the aberration kernel matrix for polarization s = +2
+        SM2 : the aberration kernel matrix for polarization s = -2
+        '''
     
     #setup all 6 HDUs with their respective keywords
     #*note that the keyword for the aberration kernel matrix is "primary"
@@ -69,6 +69,30 @@ def init_kernel_fits(kernel_file_name):
     
     hdus = [T_hdu,E_hdu,B_hdu,SP2_hdu,SM2_hdu]
     
+    #concatenate the HDUs into an HDUList and write to fits file
+    hdulist = fits.HDUList(hdus)
+    hdulist.writeto(str(kernel_file_name),overwrite=True)
+
+def init_kernel_fits(kernel_file_name):
+    '''initialize a fits file with 5 HDUs in the following order:
+        PRIMARY (T): the aberration kernel matrix for T
+        E : the aberration kernel matrix for polarization E mode
+        B : the aberration kernel matrix for polarization B mode
+        SP2 : the aberration kernel matrix for polarization s = +2
+        SM2 : the aberration kernel matrix for polarization s = -2
+        '''
+    
+    #setup all 6 HDUs with their respective keywords
+    #*note that the keyword for the aberration kernel matrix is "primary"
+    T_hdu = fits.PrimaryHDU()
+    T_hdu.name = "D1"
+    #E_hdu = fits.ImageHDU(name="E")
+    #B_hdu = fits.ImageHDU(name="B")
+    #SP2_hdu = fits.ImageHDU(name="SP2")
+    #SM2_hdu = fits.ImageHDU(name="SM2")
+    
+    #hdus = [T_hdu,E_hdu,B_hdu,SP2_hdu,SM2_hdu]
+    hdus = [T_hdu]
     #concatenate the HDUs into an HDUList and write to fits file
     hdulist = fits.HDUList(hdus)
     hdulist.writeto(str(kernel_file_name),overwrite=True)
@@ -88,19 +112,19 @@ def init_matrices_fits(matrices_file_name):
     M_hdu.name = "M"
     #LP_hdu = fits.ImageHDU(name="LP")
     L_hdu = fits.ImageHDU(name="L")
-    CS0_hdu = fits.ImageHDU(name="CS0")
-    CS2_hdu = fits.ImageHDU(name="CS2")
-    S_hdu = fits.ImageHDU(name="S")
+    #CS0_hdu = fits.ImageHDU(name="CS0")
+    #CS2_hdu = fits.ImageHDU(name="CS2")
+    #S_hdu = fits.ImageHDU(name="S")
     
     #hdus = [M_hdu,LP_hdu,L_hdu,CS0_hdu,CS2_hdu,S_hdu]
-    hdus = [M_hdu,L_hdu,CS0_hdu,CS2_hdu,S_hdu]
+    hdus = [M_hdu,L_hdu]
     
     #concatenate the HDUs into an HDUList and write to fits file
     hdulist = fits.HDUList(hdus)
     hdulist.writeto(str(matrices_file_name),overwrite=True)
 
 
-    
+
 
 
 #######################################################
@@ -108,7 +132,7 @@ def init_matrices_fits(matrices_file_name):
 #######################################################
 
 
-def save_kernel(kernel_file_name, kernel, key='T',overwrite=False):
+def save_kernel(kernel_file_name, kernel, key='D1', overwrite=False):
     '''saves the kernel chosen by 'key' to the fits file
         initializes the fits file if it doesn't already exist'''
     
@@ -127,7 +151,7 @@ def save_kernel(kernel_file_name, kernel, key='T',overwrite=False):
 
 def save_matrices(matrices_file_name, matrix, key='M',overwrite=False):
     '''saves the matrix chosen by 'key' to the fits file
-    initializes the fits file if it doesn't already exist'''
+        initializes the fits file if it doesn't already exist'''
     
     
     #check to see if the file exists
@@ -142,8 +166,17 @@ def save_matrices(matrices_file_name, matrix, key='M',overwrite=False):
     kernel_hdul[key].data = matrix
     kernel_hdul.close()
 
-
-def load_kernel(kernel_file_name, key='T'):
+def append_kernel(kernel_file_name,kernel,key):
+    kernel_hdul = fits.open(str(kernel_file_name),mode='append')
+    
+    hdu_d = fits.ImageHDU(name=key)
+    kernel_hdul.append(hdu_d)
+    kernel_hdul[key].data = kernel
+    kernel_hdul.close()
+        
+        
+        
+def load_kernel(kernel_file_name, key='D1'):
     '''loads the matrix chosen by 'key' from fits file'''
     
     
@@ -152,9 +185,9 @@ def load_kernel(kernel_file_name, key='T'):
         kernel_hdul = fits.open(str(kernel_file_name),mode='readonly')
         matrix = kernel_hdul[key].data
         kernel_hdul.close()
-
+        
         return matrix
-
+    
     #raise error if the file does not exist
     except IOError:
         print str(kernel_file_name)+" does not exist."
