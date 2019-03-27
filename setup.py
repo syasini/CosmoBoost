2abd7a9c4563bf5563528b85a5618b0f9b608485
Siavash Yasini <yasini@usc.edu>
2019-03-27 15:44:05 -0700

    update the url on setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='cosmoboost',
       version='0.2',
       description='a python package for boosting the cosmos!',
-      url='http://github.com/syasini/cosmoboost',
+      url='https://gitlab.com/siavash.yasini/CosmoBoost',
       author='Siavash Yasini',
       author_email='yasini@usc.edu',
       license='MIT',
