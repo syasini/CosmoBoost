524df149d28f383e91dc91333d3a4c3d413c33ed
Siavash Yasini <yasini@usc.edu>
2019-03-27 15:38:54 -0700

    initialize setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cosmoboost',
+      version='0.2',
+      description='a python package for boosting the cosmos!',
+      url='http://github.com/syasini/cosmoboost',
+      author='Siavash Yasini',
+      author_email='yasini@usc.edu',
+      license='MIT',
+      packages=['cosmoboost'])
