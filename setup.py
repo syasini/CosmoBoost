02a5ef3fcf4d30a818c2ce82511dc4502238d6c4
Siavash Yasini <siavash.yasini@gmail.com>
2020-09-24 17:00:52 -0700

    bump to v1.1.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='cosmoboost',
-      version='1.1.6',
+      version='1.1.7',
       description='a python package for boosting the cosmos!',
       url='https://github.com/syasini/CosmoBoost',
       install_requires=reqs,
